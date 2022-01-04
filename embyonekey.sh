c27be26fbf831d5fc038d06495571bdd0b077f4d
zzz20475 <zzz520475@gmail.com>
2022-01-04 13:50:20 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -3,6 +3,6 @@ cd /var/packages/EmbyServer/target/mono/bin
 curl https://github.com/zzz20475/embyonekey/blob/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/Ethan-7seng/embyonekey/master/user.conf
+wget https://github.com/zzz20475/embyonekey/blob/master/user.conf
 nginx -s reload
 
